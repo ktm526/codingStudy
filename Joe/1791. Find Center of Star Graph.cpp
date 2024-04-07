04b0210b6936c78941491e11c841272db03f97a9
KoreanJoey <165756862+KoreanJoey@users.noreply.github.com>
2024-04-07 18:08:00 +1000

    1791. crazy solution

--- Joe/1791. Find Center of Star Graph.cpp
+++ Joe/1791. Find Center of Star Graph.cpp
@@ -2,25 +2,21 @@
 class Solution {
 public:
     int findCenter(std::vector<std::vector<int>>& edges) {
-       int countNode = edges.size() + 1;
-       int centerNum = -1; 
-       std::vector<int> allRelation;
-       for (auto i: edges) {
-            allRelation.push_back(i[0]);
-            allRelation.push_back(i[1]);
-       }
-       for (int j = 1; j<=countNode; j++) {
-            for (auto k: allRelation) {
-                int check = 0;
-                if (j==k) {
-                    check++;
-                }
-                if (check==edges.size()) {
-                    centerNum = j;
-                    break;
-                }
-            }
-       }
-       return centerNum;
+        int a = edges[0][0];
+        int b = edges[0][1];
+        int result = -1;
+        if (edges[1][0]==a) {
+            result = a;
+        }
+        if (edges[1][0]==b) {
+            result = b;
+        }
+        if (edges[1][1]==a) {
+            result = a;
+        }
+        if (edges[1][1]==b) {
+            result = b;
+        }
+        return result;
     }
 };
